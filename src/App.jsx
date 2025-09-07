05a472947698d660bd2671260e40e70c4fabef32
Aryan <aryanrajput039@gmail.com>
2025-09-07 11:24:55 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import About from './components/About'
 import Projects from './components/Projects.jsx'
 import Skills from './components/Skills.jsx'
 import Connect from './components/Connect.jsx'
+import Loader from './components/Loader.jsx'
 
 function App() {  
 
+
+  useEffect(() => {
+    window.onload = function() {      
+      const loader = document.querySelector('.spinner-container');
+      loader.style.opacity = 0;
+      setTimeout(() => {
+        loader.style.display = 'none';
+      }, 1000);
+    }
+  }, [])
+
   return (
     <>
+      <Loader />
       <Navbar />
       <About />
       <Projects />
